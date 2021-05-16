8146bc8f57f86122dc976e2147f482dd911c262e
llennairek <llennairek@gmail.com>
2021-05-16 22:35:17 +0200

    changed routes names in english

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ItemFlip from "../components/Item/ItemFlip";
 import "./Favorites.css";
 
